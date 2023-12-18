2ff3c0c3e7e9d80228e78b980749d08cf4e58b52
rudynunez <rudy.nunez@gmail.com>
2023-12-18 01:19:57 -0500

    5. Character Animations

--- public/scripts/Person.js
+++ public/scripts/Person.js
@@ -15,6 +15,7 @@ class Person extends GameObject {
 
     update(state) {
         this.updatePosition();
+        this.updateSprite(state);
 
         if (this.isPlayerControlled && this.movingProgressRemaining === 0 && state.arrow) {
             this.direction = state.arrow;
@@ -29,4 +30,17 @@ class Person extends GameObject {
             this.movingProgressRemaining -= 1;
         }
     }
+
+    updateSprite(state) {
+
+        if (this.isPlayerControlled && this.movingProgressRemaining === 0 && !state.arrow) {
+            this.sprite.setAnimation("idle-"+this.direction);
+            return;
+        }
+       
+        if (this.movingProgressRemaining > 0) {
+            this.sprite.setAnimation("walk-"+this.direction);
+        }
+    }
+
 }
