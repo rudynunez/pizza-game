a565afc751c3058779897319c70eff2edb7a19bd
rudynunez <rudy.nunez@gmail.com>
2023-12-18 20:47:08 -0500

    6. Camera

--- public/scripts/Overworld.js
+++ public/scripts/Overworld.js
@@ -12,19 +12,27 @@ startGameLoop() {
         // Clear the canvas
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        // Draw Lower Layer
-        this.map.drawLowerImage(this.ctx);
+        // Establish the camera person
+        const cameraPerson = this.map.gameObjects.hero;
 
-        // Draw Game Objects
+        // Update all objects
         Object.values(this.map.gameObjects).forEach(object => {
             object.update({
                 arrow: this.directionInput.direction
             });
-            object.sprite.draw(this.ctx);
+        })
+
+        // Draw Lower Layer
+        this.map.drawLowerImage(this.ctx, cameraPerson);
+
+        // Draw Game Objects
+        Object.values(this.map.gameObjects).forEach(object => {
+
+            object.sprite.draw(this.ctx, cameraPerson);
         })
 
         // Draw Upper Layer
-        this.map.drawUpperImage(this.ctx);
+        this.map.drawUpperImage(this.ctx, cameraPerson);
 
         requestAnimationFrame(() => {
             step();
